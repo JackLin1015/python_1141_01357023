96bc8a3dfbbd3450a64b415448028ea2650c55e4
jacklin1015 <jack20051015@gmail.com>
2025-10-13 16:21:54 +0800

    add lab4-1.py lab4-2.py

--- week4/lab4-1.py
+++ week4/lab4-1.py
@@ -6,23 +6,26 @@ def operationcount(sum,operation,carry,count):
         else:
             carry = 0 
     return operation  
-list1 = list(map(int, input().split()))
-list1_0 = []
-list1_1 = []
-count = 0
-tool1 = 1
-operation = 0
-while(list1[0] / tool1 != 0):
-    list1_0.append(list1[0] // tool1  % 10)
-    list1_1.append(list1[1] // tool1  % 10)
-    count += 1
-    tool1 *= 10
-sum = list(map(lambda x,y: x+y,list1_0,list1_1))
-carry = 0
-result = operationcount(sum,operation,carry,count)
-if(result > 1):
-    print(f"{result} carry operations")
-elif (result == 1):
-    print("1 carry operation")
-else:
-    print("No carry operation")
+while True:
+    list1 = list(map(int, input().split()))
+    list1_0 = []
+    list1_1 = []
+    count = 0
+    tool1 = 1
+    operation = 0
+    if(list1[0] == 0 and list1[1] == 0):
+        break
+    while(list1[0] / tool1 != 0):
+        list1_0.append(list1[0] // tool1  % 10)
+        list1_1.append(list1[1] // tool1  % 10)
+        count += 1
+        tool1 *= 10
+    sum = list(map(lambda x,y: x+y,list1_0,list1_1))
+    carry = 0
+    result = operationcount(sum,operation,carry,count)
+    if(result > 1):
+        print(f"{result} carry operations.")
+    elif (result == 1):
+        print("1 carry operation.")
+    else:
+        print("No carry operation.")
--- week4/lab4-2.py
+++ week4/lab4-2.py
@@ -1,17 +1,21 @@
+def max_cola(list1, count):
+    for i in range(count):
+        n = list1[i]
+        total = n
+        empty = n
+        while empty >= 3:
+            new = empty // 3
+            total += new
+            empty = empty % 3 + new
+        if empty == 2:
+            total += 1  
+        print(total)
 list1 = []
-count  = 0
+count = 0
 while True:
     num = int(input())
     if num == 0:
-        break    
+        break
     list1.append(num)
     count += 1
-for i in range(count):
-    sum = 0
-    faken = list1[i]
-    while(faken % 3 != 0):
-        faken += 1
-    while(faken != 0):
-        sum += faken // 3
-        faken //= 3
-    print(sum + list1[i])
+max_cola(list1, count)
