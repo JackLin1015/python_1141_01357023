3b55fccd0b54e617eff9390afd62962047b70f81
jacklin1015 <jack20051015@gmail.com>
2025-10-26 15:07:53 +0800

    add

--- week3/lab3-2.py
+++ week3/lab3-2.py
@@ -1,16 +1,15 @@
-n = int(input())
 while True:
+    n = int(input())
     if n == 0:
         break
-    while True:
-        train = list(map(int, input().split()))
-        if train == [0]:
-            break
-        stack = []
-        index = 0 
-        for i in range(1, n + 1):  
-            stack.append(i)
-            while stack and stack[-1] == train[index]:
-                stack.pop()
-                index += 1
-        print("YES" if not stack else "NO")
+    train = list(map(int, input().split()))
+    if train == [0]:
+        break
+    stack = []
+    index = 0 
+    for i in range(1, n + 1):  
+        stack.append(i)
+        while stack and stack[-1] == train[index]:
+            stack.pop()
+            index += 1
+    print("YES" if not stack else "NO")
